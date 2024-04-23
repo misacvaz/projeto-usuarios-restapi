d8eb607b1b91d1435e0852b131192ace81bbf96d
misacvaz <misacvaz@gmail.com>
2024-04-22 21:45:17 -0400

    update

--- utils/validator.js
+++ utils/validator.js
@@ -5,11 +5,11 @@ module.exports = {
         req.assert('_name', 'O nome é obrigatório.').notEmpty();
         req.assert('_email', 'O e-mail está inválido.').notEmpty().isEmail();
 
-        let errors = req.validationErrors();
+        let errors = req.validationErrors()
 
         if (errors) {
 
-            app.utils.error.send(errors, req, res);
+            app.utils.error.send(errors, req, res)
             return false;
 
         } else {
